a6b02d89ef16b2dcd1c27dba58607e47ff5bd010
Animesh Trivedi <atrivedi@apache.org>
2019-09-04 18:39:32 +0200

    New build with the right headers.
    see https://stackoverflow.com/questions/56975861/error-compiling-ebpf-c-code-out-of-kernel-tree
    https://www.kernel.org/doc/html/latest/bpf/bpf_devel_QA.html#q-clang-flag-for-target-bpf
    
    all kernel build should come from /usr/src/linux-headers-`uname -r`/ locations.
    
    Signed-off-by: Animesh Trivedi <atrivedi@apache.org>

--- out-of-tree/latest/src/make.sh
+++ out-of-tree/latest/src/make.sh
@@ -1,2 +1,37 @@
-clang -target bpf -nostdinc -I/usr/include/ -I/usr/include/x86_64-linux-gnu/ -I/usr/src/linux-headers-4.15.0-1043-oem/include/ -I../common/ -O2 -D__KERNEL__ -Wno-unused-value -Wno-pointer-sign -D__TARGET_ARCH_x86 -Wno-compare-distinct-pointer-types -Wno-gnu-variable-sized-type-not-at-end -Wno-address-of-packed-member -Wno-tautological-compare -Wno-unknown-warning-option -emit-llvm -c cpustat_kern.c -o - | llc -march=bpf -filetype=obj -o cpustat_kern.o
-gcc cpustat_user.c ../common/bpf_load.c ~/vu/github/libbpf/src/libbpf.a -I/home/atr/vu/github/libbpf/include/uapi/ -I/home/atr/vu/github/libbpf/src/ -I../common/ -lelf -o cpustat
+#!/bin/bash
+#clang -target bpf , see https://www.kernel.org/doc/html/latest/bpf/bpf_devel_QA.html#q-clang-flag-for-target-bpf
+# also : https://stackoverflow.com/questions/56975861/error-compiling-ebpf-c-code-out-of-kernel-tree
+
+C_FLAGS="-Wno-unused-value -Wno-pointer-sign -Wno-compare-distinct-pointer-types -Wno-gnu-variable-sized-type-not-at-end -Wno-address-of-packed-member -Wno-tautological-compare -Wno-unknown-warning-option"
+
+function magic_make {
+DEFAULT=$1
+KERN="$DEFAULT"_kern
+USER="$DEFAULT"_user
+
+clang -nostdinc -isystem `clang -print-file-name=include` \
+	-D__KERNEL__ -D__ASM_SYSREG_H -D__TARGET_ARCH_x86 \
+	$C_FLAGS \
+	-I../common/ \
+	-include /usr/src/linux-headers-`uname -r`/include/linux/kconfig.h \
+	-I/usr/src/linux-headers-`uname -r`/include/ \
+	-I/usr/src/linux-headers-`uname -r`/include/uapi/ \
+	-I/usr/src/linux-headers-`uname -r`/include/generated/uapi/ \
+	-I/usr/src/linux-headers-`uname -r`/arch/x86/include/ \
+	-I/usr/src/linux-headers-`uname -r`/arch/x86/include/uapi/ \
+	-I/usr/src/linux-headers-`uname -r`/arch/x86/include/generated/ \
+	-I/usr/src/linux-headers-`uname -r`/arch/x86/include/generated/uapi/ \
+	-O2 -emit-llvm -c "$KERN".c -o -| llc -march=bpf -filetype=obj -o "$KERN".o
+
+gcc "$USER".c ../common/bpf_load.c ~/vu/github/libbpf/src/libbpf.a -I/home/atr/vu/github/libbpf/include/uapi/ -I/home/atr/vu/github/libbpf/src/ -I../common/ -lelf -o $DEFAULT
+
+}
+
+targets=( cpustat tracex1 )
+
+for t in "${targets[@]}" ; do
+	echo "making ...$t"
+	magic_make $t
+done
+
+
