1f4b39c1bd12f005e07be337b8af0d637b43d0d0
Animesh Trivedi <atrivedi@apache.org>
2019-09-23 19:00:22 +0200

    all user options at the top
    Signed-off-by: Animesh Trivedi <atrivedi@apache.org>

--- out-of-tree/latest/src/make.sh
+++ out-of-tree/latest/src/make.sh
@@ -5,27 +5,31 @@
 C_FLAGS="-Wno-unused-value -Wno-pointer-sign -Wno-compare-distinct-pointer-types -Wno-gnu-variable-sized-type-not-at-end -Wno-address-of-packed-member -Wno-tautological-compare -Wno-unknown-warning-option"
 
 function magic_make {
+# You can configure these two params 
+LINUX_HDRS_ROOT="/usr/src/linux-headers-`uname -r`"
+LIBBPF_ROOT="/home/atr/vu/github/libbpf"
+
 DEFAULT=$1
 KERN="$DEFAULT"_kern
 USER="$DEFAULT"_user
-LIBBPF_ROOT="/home/atr/src/libbpf"
 LIBBPF_A="$LIBBPF_ROOT/src/libbpf.a"
 LIBBPF_UAPI="$LIBBPF_ROOT/include/uapi/"
 LIBBPF_SRC="$LIBBPF_ROOT/src/"
-
+set -x 
 clang -nostdinc -isystem `clang -print-file-name=include` \
 	-D__KERNEL__ -D__ASM_SYSREG_H -D__TARGET_ARCH_x86 \
 	$C_FLAGS \
 	-I../common/ \
-	-include /usr/src/linux-headers-`uname -r`/include/linux/kconfig.h \
-	-I/usr/src/linux-headers-`uname -r`/include/ \
-	-I/usr/src/linux-headers-`uname -r`/include/uapi/ \
-	-I/usr/src/linux-headers-`uname -r`/include/generated/uapi/ \
-	-I/usr/src/linux-headers-`uname -r`/arch/x86/include/ \
-	-I/usr/src/linux-headers-`uname -r`/arch/x86/include/uapi/ \
-	-I/usr/src/linux-headers-`uname -r`/arch/x86/include/generated/ \
-	-I/usr/src/linux-headers-`uname -r`/arch/x86/include/generated/uapi/ \
+	-include $LINUX_HDRS_ROOT/include/linux/kconfig.h \
+	-I$LINUX_HDRS_ROOT/include/ \
+	-I$LINUX_HDRS_ROOT/include/uapi/ \
+	-I$LINUX_HDRS_ROOT/include/generated/uapi/ \
+	-I$LINUX_HDRS_ROOT/arch/x86/include/ \
+	-I$LINUX_HDRS_ROOT/arch/x86/include/uapi/ \
+	-I$LINUX_HDRS_ROOT/arch/x86/include/generated/ \
+	-I$LINUX_HDRS_ROOT/arch/x86/include/generated/uapi/ \
 	-O2 -emit-llvm -c "$KERN".c -o -| llc -march=bpf -filetype=obj -o "$KERN".o
+set +x 
 
 gcc "$USER".c ../common/bpf_load.c $LIBBPF_A -I$LIBBPF_UAPI -I$LIBBPF_SRC -I../common/ -lelf -o $DEFAULT
 
